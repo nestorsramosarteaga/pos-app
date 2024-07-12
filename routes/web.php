bcf4263eaac00297db596277d7a650180562c9a6
Nestor Ramos <nestorsramosarteaga@gmail.com>
2024-07-12 15:32:21 -0500

    Add compras routes

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\MarcaController;
+use App\Http\Controllers\CompraController;
 use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\ProductoController;
-use App\Http\Controllers\ProveedorController;
 use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ProveedorController;
 use App\Http\Controllers\PresentacionController;
 /*
 |--------------------------------------------------------------------------
@@ -33,6 +34,7 @@ Route::resources([
     'productos' => ProductoController::class,
     'clientes' => ClienteController::class,
     'proveedores' => ProveedorController::class,
+    'compras' => CompraController::class,
 ]);
 
 Route::get('/login', function () {
